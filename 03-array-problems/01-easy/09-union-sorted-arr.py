e18bef42c8f6932a16d32b37c7b9711533866315
Da-Soumalya <dsoumalya630@gmail.com>
2026-04-04 19:13:19 +0530

    improved the code writing style for a file...

--- 03-array-problems/01-easy/09-union-sorted-arr.py
+++ 03-array-problems/01-easy/09-union-sorted-arr.py
@@ -3,34 +3,36 @@ Input: n = 10, m = 7, arr1[] = {1,2,3,4,5,6,7,8,9,10} arr2[] = {2,3,4,4,5,11,12}
 '''
 
 def union_sorted(arr1, arr2):
-    arr = [float('inf')]
-    i , j, prev = 0, 0, 0
-    while i != len(arr1) and j < len(arr2):
-        if arr1[i] <= arr2[j] and arr[prev] != arr1[i]:
-            arr.append(arr1[i])
+    i, j = 0, 0
+    arr = []
+
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] == arr2[j]:
+            if not arr or arr[-1] != arr1[i]:
+                arr.append(arr1[i])
             i += 1
-            prev += 1
-            continue
-        if arr2[j] < arr1[i] and arr[prev] != arr2[j]:
-            arr.append(arr2[j])
             j += 1
-            prev += 1
-            continue
-        if arr1[i] < arr2[j]:
+
+        elif arr1[i] < arr2[j]:
+            if not arr or arr[-1] != arr1[i]:
+                arr.append(arr1[i])
             i += 1
+
         else:
+            if not arr or arr[-1] != arr2[j]:
+                arr.append(arr2[j])
             j += 1
-    
-    while i != len(arr1):
-        if arr1[i] != arr[prev]:
+
+    while i < len(arr1):
+        if not arr or arr[-1] != arr1[i]:
             arr.append(arr1[i])
         i += 1
-    while j != len(arr2):
-        if arr2[j] != arr[prev]:
+
+    while j < len(arr2):
+        if not arr or arr[-1] != arr2[j]:
             arr.append(arr2[j])
         j += 1
 
-    arr.pop(0)
     return arr
     
 
